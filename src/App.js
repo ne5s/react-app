b6dfbb907136477d0e896863e9ce9780e5c365b6
ne5s <phobos90@naver.com>
2022-06-08 14:48:17 +0900

    2022-06-08 commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { useState } from "react";
 
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">Web</a>
+        <a
+          href="/"
+          onClick={(evt) => {
+            console.log("evt", evt);
+            evt.preventDefault();
+            props.onSelect();
+          }}
+        >
+          WWW
+        </a>
       </h1>
     </header>
   );
 }
 
 function Nav(props) {
-  console.log(props.data);
+  // console.log(props.data);
   const list = props.data.map((e) => {
     return (
       <li key={e.id}>
-        <a href={"/read/" + e.id}>{e.title}</a>
+        <a
+          href={"/read/" + e.id}
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onClick(e.id);
+          }}
+        >
+          {e.title}
+        </a>
       </li>
     );
   });
-  // const list = [
-  //   <li>
-  //     <a href="/read/1">{props.data[0].title}</a>
-  //   </li>,
-  //   <li>
-  //     <a href="/read/2">{props.data[1].title}</a>
-  //   </li>,
-  // ];
   return (
     <nav>
       <ol>{list}</ol>
@@ -44,16 +56,57 @@ function Article(props) {
 }
 
 function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  console.log(mode, id);
   const topics = [
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
   ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
+  } else if (mode === "READ") {
+    const topic = topics.filter((e) => {
+      if (e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    content = <Article title={topic.title} body={topic.body}></Article>;
+  }
+
   return (
     <div>
-      <Header></Header>
-      <Nav data={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB!"></Article>
-      <Article title="HTML" body="HTML is ..."></Article>
+      <Header
+        onSelect={() => {
+          // mode = "WELCOME";
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        data={topics}
+        onClick={(id) => {
+          // mode = "READ";
+          setMode("READ");
+          setId(id);
+        }}
+      ></Nav>
+      {content}
+      <br></br>
+      <ButtonGroup>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            alert("create!");
+          }}
+        >
+          Create
+        </Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
