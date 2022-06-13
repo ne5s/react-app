4ff044d947ce1957f5e7494a79078ae8a1fabbdc
ne5s <phobos90@naver.com>
2022-06-13 14:31:59 +0900

    create 기능 구현

--- 0613/src/App.js
+++ 0613/src/App.js
@@ -2,15 +2,11 @@ import "./App.css";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { useState } from "react";
+import styled from "styled-components";
 
 function Header(props) {
-  const myStyle = {
-    borderBottom: "1px solid gray",
-    padding: "10px",
-    fontSize: "20px",
-  };
   return (
-    <header style={myStyle}>
+    <header className={props.className}>
       <h1>
         <a
           href="/"
@@ -26,6 +22,10 @@ function Header(props) {
     </header>
   );
 }
+
+const HeaderStyled = styled(Header)`
+  border-bottom: 1px solid gray;
+`;
 
 function Nav(props) {
   // console.log(props.data);
@@ -60,14 +60,41 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          const title = evt.target.title.value;
+          const body = evt.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title"></input>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
   console.log(mode, id);
-  const topics = [
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
-  ];
+  ]);
   let content = null;
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, WEB!"></Article>;
@@ -80,16 +107,32 @@ function App() {
       }
     })[0];
     content = <Article title={topic.title} body={topic.body}></Article>;
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(title, body) => {
+          const newTopic = { id: nextId, title, body };
+          // topics.push(newTopic);
+          // setTopics(topics);
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setId(nextId);
+          setMode("READ");
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
 
   return (
     <div>
-      <Header
+      <HeaderStyled
         onSelect={() => {
           // mode = "WELCOME";
           setMode("WELCOME");
         }}
-      ></Header>
+      ></HeaderStyled>
       <Nav
         data={topics}
         onClick={(id) => {
@@ -104,7 +147,7 @@ function App() {
         <Button
           variant="outlined"
           onClick={() => {
-            alert("create!");
+            setMode("CREATE");
           }}
         >
           Create
