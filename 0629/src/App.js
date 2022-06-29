35a6a8f9c46521af126c90b954978265f47b48c4
ne5s <phobos90@naver.com>
2022-06-29 11:04:35 +0900

    Nav 골격 생성

--- 0629/src/App.js
+++ 0629/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Header } from "./Header";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <Link to="/read/1">html</Link>
+        </li>
+      </ol>
+    </nav>
+  );
+}
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+    </div>
+  );
 }
 
 export default App;
