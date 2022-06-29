2c62223aef43df5c46f075446285f08ca46c234a
ne5s <phobos90@naver.com>
2022-06-29 11:43:01 +0900

    topics 데이터 가져오기

--- 0629/src/App.js
+++ 0629/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Header } from "./Header";
 import { Link, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Welcome } from "./Welcome";
 
 function Nav() {
   return (
@@ -23,20 +25,24 @@ function Read() {
   );
 }
 
-function Welcome() {
-  return (
-    <article>
-      <h2>Welcome</h2>
-      Hello, WEB
-    </article>
-  );
-}
-
 function App() {
+  const [topics, setTopics] = useState([]);
+  async function refresh() {
+    const resp = await fetch("http://localhost:3333/topics");
+    const data = await resp.json();
+    console.log(data);
+    setTopics((topics) => data);
+  }
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  const ui = topics.map((e) => <li key={e.id}>{e.title}</li>);
   return (
     <div className="App">
       <Header></Header>
       <Nav></Nav>
+      <ol>{ui}</ol>
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="read/1" element={<Read />} />
